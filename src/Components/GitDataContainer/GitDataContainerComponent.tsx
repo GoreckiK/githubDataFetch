492250b3b103e12a68203574b9a571f2bd3b2236
Krzysztof Górecki <kgorecki@MacBook-Pro-macbookpro.local>
2020-02-05 11:01:14 +0100

    localStorage state update on init pagination added

--- src/Components/GitDataContainer/GitDataContainerComponent.tsx
+++ src/Components/GitDataContainer/GitDataContainerComponent.tsx
@@ -1,56 +1,54 @@
-import React, {ChangeEvent, FormEvent} from "react";
+import React, {FormEvent} from "react";
 import {TableComponent} from "../TableComponent/TableComponent";
 import './GitDataContainerComponent.css';
 import {RepositoryDataModel} from "../TableComponent/repository-data-model";
 import {TextField} from "@material-ui/core";
 import {getRepositoriesByName} from "../../API";
-import _ from 'lodash';
+import { debounce } from 'lodash';
 
-export class GitDataContainerComponent extends React.Component<{}, {gitHubData: RepositoryDataModel[], searchedName: string}> {
+export class GitDataContainerComponent extends React.Component<{}, {gitHubData: RepositoryDataModel[], latestSearch: string}> {
     constructor(props: any) {
         super(props);
         this.state = {
             gitHubData: [],
-            searchedName: ''
+            latestSearch: ''
         };
-        this.onFormSubmit = _.debounce(this.onFormSubmit.bind(this), 250);
+        this.onChange = debounce(this.onChange.bind(this), 350);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        const query = localStorage.getItem('latestSearch') || '';
+        const latestSearchData = localStorage.getItem(query);
+        if (query) this.setState({
+            gitHubData: typeof(latestSearchData) === 'string' ? JSON.parse(latestSearchData) : []
+        })
     }
 
     onFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
-        // e.preventDefault();
-        // if (localStorage.getItem('searchedName') !== this.state.searchedName) {
-        //     getRepositoriesByName(this.state.searchedName)
-        //         .then(res => {
-        //             // @ts-ignore
-        //             localStorage.setItem('searchedName', this.state.searchedName);
-        //             this.setState({
-        //                 gitHubData: res.data.items
-        //             });
-        //         })
-        //         .catch(err => console.error(err));
-        // }
+        e.preventDefault();
     };
 
-    onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        debugger;
-        // this.setState({
-        //     searchedName: e.target.value
-        // });
-        e.preventDefault();
-        if (!localStorage.getItem(e.target.value)) {
-            getRepositoriesByName(e.target.value)
+    onChange = (value: string): void => {
+        if (value && !localStorage.getItem(value)) {
+            getRepositoriesByName(value)
                 .then(res => {
                     // @ts-ignore
-                    localStorage.setItem(e.target.value, e.target.value);
+                    localStorage.setItem(value, JSON.stringify(res.data.items));
+                    localStorage.setItem('latestSearch', value);
                     this.setState({
-                        gitHubData: res.data.items
+                        gitHubData: res.data.items,
+                        latestSearch: value
                     });
                 })
                 .catch(err => console.error(err));
         } else {
-                // @ts-ignore
+            // @ts-ignore
+            const localStorageData = JSON.parse(localStorage.getItem(value));
+            localStorage.setItem('latestSearch', value);
             this.setState({
-                gitHubData: localStorage.getItem(e.target.value)
+                gitHubData: localStorageData,
+                latestSearch: value
             });
         }
     };
@@ -58,8 +56,8 @@ export class GitDataContainerComponent extends React.Component<{}, {gitHubData: 
     render() {
         return (
             <div className="git-table">
-                <form className="search-form">
-                    <TextField id="repo-search" variant="filled" size="small" onChange={this.onChange}/>
+                <form className="search-form" onSubmit={this.onFormSubmit}>
+                    <TextField id="repo-search" variant="filled" size="small" onChange={({target: {value}}) => this.onChange(value)}/>
                 </form>
                 <TableComponent repoData={this.state.gitHubData}/>
             </div>
--- src/Components/TableComponent/TableComponent.tsx
+++ src/Components/TableComponent/TableComponent.tsx
@@ -1,33 +1,58 @@
-import React from 'react'
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import React, { useState } from 'react'
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination} from "@material-ui/core";
 import './TableComponent.css';
 import {RepositoryDataModel} from "./repository-data-model";
 
 export const TableComponent = (props: {repoData: RepositoryDataModel[]}) => {
+    const [rowsPerPage, setRowsPerPage] = useState(5)
+    const [page, setPage] = useState(0)
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void | undefined => {
+        setRowsPerPage(parseInt(event.target.value));
+    }
+
+    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, newPage: number): void => {
+        setPage(newPage)
+    }
     return (
-        <TableContainer component={Paper}>
-            <Table className='table'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Repo Title</TableCell>
-                        <TableCell>Owner</TableCell>
-                        <TableCell>Stars</TableCell>
-                        <TableCell>Created at</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.repoData.map(row => (
-                        <TableRow key={row.id}>
-                            <TableCell>{row.id}</TableCell>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.owner.login}</TableCell>
-                            <TableCell>{row.stargazers_count}</TableCell>
-                            <TableCell>{row.created_at.slice(0, 10)}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div>
+            <Paper className='paper'>
+                <TableContainer>
+                    <Table className='table'>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Repo Title</TableCell>
+                                <TableCell>Owner</TableCell>
+                                <TableCell>Stars</TableCell>
+                                <TableCell>Created at</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {props.repoData
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map(row => (
+                                <TableRow key={row.id}>
+                                    <TableCell>{row.id}</TableCell>
+                                    <TableCell>{row.name}</TableCell>
+                                    <TableCell>{row.owner.login}</TableCell>
+                                    <TableCell>{row.stargazers_count}</TableCell>
+                                    <TableCell>{row.created_at.slice(0, 10)}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 15, 20]}
+                    component="div"
+                    count={props.repoData.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </div>
     )
 };
