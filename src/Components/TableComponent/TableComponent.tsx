09e35f36132b9d88aae9e80e258f484772bca299
Krzysztof Górecki <kgorecki@MacBook-Pro-macbookpro.local>
2020-02-05 23:02:22 +0100

    table sorting

--- src/Components/TableComponent/TableComponent.tsx
+++ src/Components/TableComponent/TableComponent.tsx
@@ -2,18 +2,45 @@ import React, { useState } from 'react'
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination} from "@material-ui/core";
 import './TableComponent.css';
 import {RepositoryDataModel} from "./repository-data-model";
-
+type Order = 'asc' | 'desc';
+interface Header {
+    
+}
 export const TableComponent = (props: {repoData: RepositoryDataModel[]}) => {
-    const [rowsPerPage, setRowsPerPage] = useState(5)
-    const [page, setPage] = useState(0)
-
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [page, setPage] = useState(0);
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');  
+    // const [sortColumn, setSortColumn] = useState();
+    // const [sortDirection, setSortDirection] = useState('desc');
+    // const invertDirection = {
+    //     asc: 'desc',
+    //     desc: 'asc'
+    // };
+    const tableHeaders = [{id: 'ID'}, {id: 'Repo Title'}, {id: 'Owner'}, {id: 'Stars'}, {id: 'Created at'}];
     const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void | undefined => {
         setRowsPerPage(parseInt(event.target.value));
-    }
+    } 
 
     const handleChangePage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, newPage: number): void => {
         setPage(newPage)
     }
+
+    // const handleSort: void = (columnName: string) => {
+    //     setSortColumn(columnName);
+    //     sortColumn === columnName ? setSortDirection(invertDirection[sortColumn]) : setSortDirection('asc');
+    // }
+
+    const TableHeadWithSorting = () => {
+        return (
+            <TableHead>
+                <TableRow>
+
+                </TableRow>
+            </TableHead>
+        )
+    }
+
     return (
         <div>
             <Paper className='paper'>
@@ -21,11 +48,11 @@ export const TableComponent = (props: {repoData: RepositoryDataModel[]}) => {
                     <Table className='table'>
                         <TableHead>
                             <TableRow>
-                                <TableCell>ID</TableCell>
-                                <TableCell>Repo Title</TableCell>
-                                <TableCell>Owner</TableCell>
-                                <TableCell>Stars</TableCell>
-                                <TableCell>Created at</TableCell>
+                                {tableHeaders.map(header => (
+                                <TableCell 
+                                    sortDirection={orderBy === header.id ? order : 'asc'}>
+                                    {header.id}</TableCell>
+                                ))}
                             </TableRow>
                         </TableHead>
                         <TableBody>
